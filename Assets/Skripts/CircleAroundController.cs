f8c3bea65083ad94398b0341a8177053e9b93d58
domin1101 <dominik.winkelbauer@outlook.de>
2016-08-04 22:49:15 +0200

    [FEATURE] Makes it possible to circle planets around other planets

---
+++ Assets/Skripts/CircleAroundController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleAroundController : MonoBehaviour {
+    
+    public GameObject center;
+    public float speed = 10;
+    public int dir = 1;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 rot = Quaternion.AngleAxis(90, Vector3.up) * (transform.position - center.transform.position);
+        rot.Normalize();
+
+        rb.velocity = rot * speed * dir;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 diff = (transform.position - center.transform.position);
+
+        float strength = speed * speed * rb.mass / diff.magnitude;
+        diff.Normalize();
+
+        rb.AddForce(-diff * strength);
+    }
+}
